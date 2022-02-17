19906392c21779bb064126cd5ac1d7e9499a21fa
Wouter <wbi@uplearning.nl>
2022-02-17 16:50:25 +0100

    script to console

--- app.py
+++ app.py
@@ -2,7 +2,22 @@
 from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 #############################
-# Additional code goes here #
+@app.route('/')
+def home_page():
+    example_embed='This string is from python'
+    return render_template('index.html', embed=example_embed)
+
+
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask!'}
+        return jsonify(message)  # serialize and use JSON headers
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
 #############################
 #########  run app  #########
 app.run(debug=True)
