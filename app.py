9ee8520614ad89b51e753a8a1c821af26e028178
Wouter <wbi@uplearning.nl>
2022-02-17 16:57:49 +0100

    added fetch from collection

--- app.py
+++ app.py
@@ -2,6 +2,12 @@
 from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
 #############################
+
+######## Example data, in sets of 3 ############
+data = list(range(1,300,3))
+print (data)
+######## End example data ############
+
 @app.route('/')
 def home_page():
     example_embed='This string is from python'
@@ -18,6 +24,18 @@ def testfn():
     if request.method == 'POST':
         print(request.get_json())  # parse as JSON
         return 'Sucesss', 200
+
+######## Data fetch ############
+@app.route('/getdata/<index_no>', methods=['GET','POST'])
+def data_get(index_no):
+    
+    if request.method == 'POST': # POST request
+        print(request.get_text())  # parse as text
+        return 'OK', 200
+    
+    else: # GET request
+        return 't_in = %s ; result: %s ;'%(index_no, data[int(index_no)])
+
 #############################
 #########  run app  #########
 app.run(debug=True)
