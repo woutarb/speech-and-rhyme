712c5a68748b5dc54e2eb75eced12c2fce53aea1
Wouter <wbi@uplearning.nl>
2022-02-21 12:30:57 +0100

    Milestone 1
    rijmwoordenboek with static variables works

--- app.py
+++ app.py
@@ -1,41 +1,18 @@
 ########  imports  ##########
 from flask import Flask, jsonify, request, render_template
 app = Flask(__name__)
+
+import rijmwoord
 #############################
 
-######## Example data, in sets of 3 ############
-data = list(range(1,300,3))
-print (data)
-######## End example data ############
-
+######## Data fetch ############
 @app.route('/')
 def home_page():
-    example_embed='This string is from python'
+    userinput ="grappig"
+    example_embed=rijmwoord.rijmwoorden(userinput)
     return render_template('index.html', embed=example_embed)
-
-
-@app.route('/test', methods=['GET', 'POST'])
-def testfn():
-    # GET request
-    if request.method == 'GET':
-        message = {'greeting':'Hello from Flask!'}
-        return jsonify(message)  # serialize and use JSON headers
-    # POST request
-    if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
-        return 'Sucesss', 200
-
-######## Data fetch ############
-@app.route('/getdata/<index_no>', methods=['GET','POST'])
-def data_get(index_no):
-    
-    if request.method == 'POST': # POST request
-        print(request.get_text())  # parse as text
-        return 'OK', 200
-    
-    else: # GET request
-        return 't_in = %s ; result: %s ;'%(index_no, data[int(index_no)])
 
 #############################
 #########  run app  #########
 app.run(debug=True)
+
---
+++ rijmwoordenboek.py
@@ -0,0 +1,69 @@
+########  imports  ##########
+from flask import Flask, jsonify, request, render_template
+app = Flask(__name__)
+#############################
+
+vowels = {'@', 'A', 'a', 'E', 'I', 'e', '|', '}', 'o', 'O', 'i', 'y', 'u', 'L', 'K', 'M'}
+
+@app.route('/test', methods=['GET', 'POST'])
+def testfn():
+    # GET request
+    if request.method == 'GET':
+        message = {'greeting':'Hello from Flask. Rijmwoordenboek is working!'}
+        return jsonify(message)  # serialize and use JSON headers
+    # POST request
+    if request.method == 'POST':
+        print(request.get_json())  # parse as JSON
+        return 'Sucesss', 200
+
+@app.route('/')
+
+def zoek_rijm(woord):
+    accent = woord.rfind('\'')
+    for i, letter in enumerate(woord[accent+1:]):
+        if letter in vowels:
+            return woord[accent+i+1:]
+    return ""
+
+def zoek_alliteratie(woord):
+    accent = woord.rfind('\'')
+    voorstuk = (woord[:accent])
+    onset = ""
+    for i, e in enumerate(voorstuk[::-1]):
+        if e in vowels: break
+        onset += e
+    return onset[::-1]
+
+
+def normaliseer(woord):
+    return woord.replace('#e','é').replace('#a','á').replace('#o','ó').replace('`e','è').replace('`a','à').replace('`o','ò').replace('`u','ù').replace('`i','ì').replace('"a','ä').replace('"e','ë').replace('"i','ï').replace('"u','ü').replace(',c','ç').replace('"o','ö').replace('^a','â').replace('^e','ê').replace('^i','î').replace('^u','û').replace('^o','ô')
+
+if __name__ == "__main__":
+
+    rijmwoordenboek = {}
+
+    with open("celex.txt","r") as celex:
+        woordenlijst = celex.readlines()
+        for regel in woordenlijst:
+            woorden = regel.split('\\')
+            rijm = zoek_rijm(woorden[2][:-1])
+            lemma = normaliseer(woorden[0].split()[-1])
+            if rijm == '': continue
+            if rijm in rijmwoordenboek:
+                rijmwoordenboek[rijm].add(lemma)
+            else: rijmwoordenboek[rijm]={lemma}
+
+    hulprijmwoordenboek = {}
+
+    for rijm in rijmwoordenboek:
+        for word in rijmwoordenboek[rijm]:
+            hulprijmwoordenboek[word]=rijm
+
+    rijmwoorden = lambda x: {w for w in rijmwoordenboek[hulprijmwoordenboek[x]] if not w == x}
+
+    print (f"# coding=utf8\nrijmwoordenboek = {rijmwoordenboek}\nhulprijmwoordenboek={hulprijmwoordenboek}\nrijmwoorden = lambda x: {{w for w in rijmwoordenboek[hulprijmwoordenboek[x]] if not w == x}}", file=open('alliteratiewoord.py','w'))
+
+    
+#############################
+#########  run app  #########
+app.run(debug=True)
