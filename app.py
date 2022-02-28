19f1e025fb3181bbba116c90c9e6965d596e645b
Wouter <wbi@uplearning.nl>
2022-02-28 20:45:38 +0100

    Great purge of non-working lines of code, returning to basics
    using request.form.get now

--- app.py
+++ app.py
@@ -7,33 +7,22 @@ import rijmwoord
 ######## Data fetch ############
 @app.route('/')
 def home_page():
-    if 'userinput1' in session:
-        userinput1 =session[userinput1]
-        example_embed=rijmwoord.rijmwoorden(userinput1)
-        return render_template('index.html', embed=example_embed)
     userinput ="voorbeeld"
     example_embed=rijmwoord.rijmwoorden(userinput)
     return render_template('index.html', embed=example_embed)
 
 
-@app.route('/do_something', methods=["POST", "GET"])
-def do_something():
-    if 'userinput1' in session:
-        userinput1 =session[userinput1]
-        example_embed=rijmwoord.rijmwoorden(userinput1)
+@app.route('/secondPage{data}', methods=["POST"])
+def checkRijm():
+    userinput=request.form.get('fword')
+    if userinput:
+        rijm_embed=rijmwoord.rijmwoorden(userinput)
+        return render_template('index.html', embed=rijm_embed)
+    else:
+        userinput ="voorbeeld"
+        example_embed=rijmwoord.rijmwoorden(userinput)
         return render_template('index.html', embed=example_embed)
-    userinput1 = request.form.get('fWord')
-    userinput2 = request.form.get('sWord')
-    print (userinput1)
-    example_embed=rijmwoord.rijmwoorden(userinput1)
-    return render_template('index.html', embed=example_embed)
-    
-with app.test_request_context():
-    print (url_for('do_something', next='/'))
+        
 #############################
 #########  run app  #########
 app.run(debug=True)
-
-
-
-
